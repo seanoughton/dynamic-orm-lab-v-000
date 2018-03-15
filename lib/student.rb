6a9f486a560495d4126e5852d5f2875b4e63dffb
Sean Oughton <seanoughton@gmail.com>
2018-03-15 20:31:32 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,11 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+
+    self.column_names.each do |column_name|
+      #turn each column name into a symbol with .to_sym
+      attr_accessor column_name.to_sym
+    end
+
+
 end
